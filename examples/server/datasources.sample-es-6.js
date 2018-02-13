1677230b684e4800b4e1ec17262e22960146939b
Bharath Reddy Kontham <bharath2211@gmail.com>
2018-02-14 00:56:21 +0530

    merge esv6 support updates (#108)
    * Specify the version of http-aws-es needed (#105)
    
    `http-aws-es` has had quite a few updates now and it does not support passing through credentials in the same manner. This could be remedied by simply creating a AWS.Config object to pass in as a parameter instead of the credentials however just updating the readme should also make it functional.
    
    * updated packages for apiVerison 6.0 support
    
    * replaced _.pluck with _.map
    
    * updated connector with elasticsearch 6.0 single index single type support
    
    * updated whereBuilder
    
    * added model differentiator using docType property
    
    * added support for combination filters with docType
    
    * added support for nested queries with dot(.) notation
    
    * added like and nlike support
    
    * added geo queries support
    
    * updated version to 1.4.5
    
    * fixed eslint issues
    
    * ElasticSearch 6.x example datasource
    
    * added intial testcases for es 6
    
    * updated filters test cases
    
    * updated automigrate script

---
+++ examples/server/datasources.sample-es-6.js
@@ -0,0 +1,103 @@
+module.exports = {
+  'db': {
+    'name': 'elasticsearch-6',
+    'connector': 'es',
+    'index': 'shakespeare',
+    'hosts': [{
+      'host': 'localhost',
+      'port': 9202
+    }],
+    'apiVersion': '6.0',
+    'log': 'trace',
+    'defaultSize': 50,
+    'requestTimeout': 30000,
+    'mappings': [],
+    "mappingType": "basedata",
+    "mappingProperties": {
+      "id": {
+        "type": "keyword",
+        "index": true
+      },
+      "docType": {
+        "type": "keyword",
+        "index": true
+      },
+      "name": {
+        "type": "text",
+        "index": true
+      },
+      'realm': { 
+        'type': 'keyword', 
+        'index': true 
+      },
+      'username': { 
+        'type': 'keyword', 
+        'index': true 
+      },
+      "description": {
+        "type": "text",
+        "index": true
+      },
+      "roleId": {
+        "type": "keyword",
+        "index": true
+      },
+      "principalId": {
+        "type": "keyword",
+        "index": true
+      },
+      "principalType": {
+        "type": "keyword",
+        "index": true
+      },
+      "model": {
+        "type": "keyword",
+        "index": true
+      },
+      "property": {
+        "type": "keyword",
+        "index": true
+      },
+      "accessType": {
+        "type": "keyword",
+        "index": true
+      },
+      "permission": {
+        "type": "keyword",
+        "index": true
+      },
+      "userId": {
+        "type": "keyword",
+        "index": true
+      },
+      "ttl": {
+        "type": "long",
+        "index": true
+      },
+      "email": {
+        "type": "keyword",
+        "index": true
+      },
+      "password": {
+        "type": "keyword",
+        "index": false
+      },
+      "status": {
+        "type": "keyword",
+        "index": true
+      },
+      "created": {
+        "type": "date",
+        "index": true
+      },
+      "modified": {
+        "type": "date",
+        "index": true
+      },
+      "lastUpdated": {
+        "type": "date",
+        "index": true
+      }
+    }
+  }
+};
---
+++ test/es-v6/04.add-defaults-refresh-true.test.js
@@ -0,0 +1,96 @@
+/*eslint no-console: "off"*/
+/*global getSchema should assert*/
+describe('Add Defaults', function () {
+    var testConnector, testConnector2, db, db2;
+
+    before(function (done) {
+        require('./init.js');
+        var settings = getSettings();
+        settings.log = 'error';
+        db = getDataSource(settings);
+
+        var settings2 = getSettings();
+        settings2.refreshOn = ["save", "updateAttributes"];
+        db2 = getDataSource(settings2);
+
+        var account = {real_name: {type: String, index: true, sort: true}};
+        db2.define("Account", account);
+        db.define("Account", account);
+        var bookProps = {real_name: {type: String, index: true, sort: true}};
+        var bookSettings = {
+            "properties": {
+                "real_name": {
+                    "type": "keyword"
+                }
+            },
+            "elasticsearch": {
+                "create": {
+                    "refresh": false
+                },
+                "destroy": {
+                    "refresh": false
+                },
+                "destroyAll": {
+                    "refresh": "wait_for"
+                }
+            }
+        };
+        db.define("Book", bookProps, bookSettings);
+        db2.define("Book", bookProps, bookSettings);
+        testConnector = db.connector;
+        testConnector2 = db2.connector;
+        db.automigrate(done);
+    });
+
+    describe('Datasource specific settings', function () {
+
+        it('modifying operations should have refresh true', function () {
+            (typeof testConnector2.addDefaults('Account', 'create').refresh === 'undefined').should.be.true;
+            (testConnector2.addDefaults('Account', 'save').refresh === true).should.be.true;
+            (typeof testConnector2.addDefaults('Account', 'destroy').refresh === 'undefined').should.be.true;
+            (typeof testConnector2.addDefaults('Account', 'destroyAll').refresh === 'undefined').should.be.true;
+            (testConnector2.addDefaults('Account', 'updateAttributes').refresh === true).should.be.true;
+            (typeof testConnector2.addDefaults('Account', 'updateOrCreate').refresh === 'undefined').should.be.true;
+        });
+
+        it('create and destroy should have refresh false for model book', function () {
+            (testConnector2.addDefaults('Book', 'destroy').refresh === false).should.be.true;
+            (testConnector2.addDefaults('Book', 'create').refresh === false).should.be.true;
+            (testConnector2.addDefaults('Book', 'save').refresh === true).should.be.true;
+            (testConnector2.addDefaults('Book', 'destroyAll').refresh === 'wait_for').should.be.true;
+            (testConnector2.addDefaults('Book', 'updateAttributes').refresh === true).should.be.true;
+            (typeof testConnector2.addDefaults('Book', 'updateOrCreate').refresh === 'undefined').should.be.true;
+        });
+
+        it('should never have a refresh attribute', function () {
+            (typeof testConnector.addDefaults('Book', 'removeMappings').refresh === 'undefined').should.be.true;
+            (typeof testConnector.addDefaults('Book', 'buildFilter').refresh === 'undefined').should.be.true;
+            (typeof testConnector.addDefaults('Book', 'find').refresh === 'undefined').should.be.true;
+            (typeof testConnector.addDefaults('Book', 'exists').refresh === 'undefined').should.be.true;
+            (typeof testConnector.addDefaults('Book', 'count').refresh === 'undefined').should.be.true;
+        });
+    });
+
+    describe('Model specific settings', function () {
+
+        it('modifying operations should have refresh true', function () {
+            (testConnector.addDefaults('Account', 'create').refresh === true).should.be.true;
+            (testConnector.addDefaults('Account', 'save').refresh === true).should.be.true;
+            (testConnector.addDefaults('Account', 'destroy').refresh === true).should.be.true;
+            (testConnector.addDefaults('Account', 'destroyAll').refresh === true).should.be.true;
+            (testConnector.addDefaults('Account', 'updateAttributes').refresh === true).should.be.true;
+            (testConnector.addDefaults('Account', 'updateOrCreate').refresh === true).should.be.true;
+
+        });
+
+        it('create and destroy should have refresh false for model book', function () {
+            (testConnector.addDefaults('Book', 'destroy').refresh === false).should.be.true;
+            (testConnector.addDefaults('Book', 'create').refresh === false).should.be.true;
+            (testConnector.addDefaults('Book', 'save').refresh === true).should.be.true;
+            (testConnector.addDefaults('Book', 'destroyAll').refresh === 'wait_for').should.be.true;
+            (testConnector.addDefaults('Book', 'updateAttributes').refresh === true).should.be.true;
+            (testConnector.addDefaults('Book', 'updateOrCreate').refresh === true).should.be.true;
+        });
+
+    });
+});
